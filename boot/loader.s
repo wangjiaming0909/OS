c8e7365406d9195a97663315d0a5c9c0dfb9b7c7
wangjiaming0909 <604227650@qq.com>
2017-12-05 15:29:38 +0800

    123

--- boot/loader.s
+++ boot/loader.s
@@ -1,94 +1,123 @@
-   %include "boot.inc"
-   section loader vstart=LOADER_BASE_ADDR
-   LOADER_STACK_TOP equ LOADER_BASE_ADDR
-   jmp loader_start					; 此处的物理地址是:
-   
-;构建gdt及其内部的描述符
-   GDT_BASE:   dd    0x00000000 
-	       dd    0x00000000
-
-   CODE_DESC:  dd    0x0000FFFF 
-	       dd    DESC_CODE_HIGH4
-
-   DATA_STACK_DESC:  dd    0x0000FFFF
-		     dd    DESC_DATA_HIGH4
-
-   VIDEO_DESC: dd    0x80000007	       ;limit=(0xbffff-0xb8000)/4k=0x7
-	       dd    DESC_VIDEO_HIGH4  ; 此时dpl已改为0
-
-   GDT_SIZE   equ   $ - GDT_BASE
-   GDT_LIMIT   equ   GDT_SIZE -	1 
-   times 60 dq 0					 ; 此处预留60个描述符的slot
-   SELECTOR_CODE equ (0x0001<<3) + TI_GDT + RPL0         ; 相当于(CODE_DESC - GDT_BASE)/8 + TI_GDT + RPL0
-   SELECTOR_DATA equ (0x0002<<3) + TI_GDT + RPL0	 ; 同上
-   SELECTOR_VIDEO equ (0x0003<<3) + TI_GDT + RPL0	 ; 同上 
-
-   ;以下是定义gdt的指针，前2字节是gdt界限，后4字节是gdt起始地址
-
-   gdt_ptr  dw  GDT_LIMIT 
-	    dd  GDT_BASE
-   loadermsg db '2 loader in real.'
-
-   loader_start:
-
-;------------------------------------------------------------
-;INT 0x10    功能号:0x13    功能描述:打印字符串
-;------------------------------------------------------------
-;输入:
-;AH 子功能号=13H
-;BH = 页码
-;BL = 属性(若AL=00H或01H)
-;CX＝字符串长度
-;(DH、DL)＝坐标(行、列)
-;ES:BP＝字符串地址 
-;AL＝显示输出方式
-;   0——字符串中只含显示字符，其显示属性在BL中。显示后，光标位置不变
-;   1——字符串中只含显示字符，其显示属性在BL中。显示后，光标位置改变
-;   2——字符串中含显示字符和显示属性。显示后，光标位置不变
-;   3——字符串中含显示字符和显示属性。显示后，光标位置改变
-;无返回值
-   mov	 sp, LOADER_BASE_ADDR
-   mov	 bp, loadermsg           ; ES:BP = 字符串地址
-   mov	 cx, 17			 ; CX = 字符串长度
-   mov	 ax, 0x1301		 ; AH = 13,  AL = 01h
-   mov	 bx, 0x001f		 ; 页号为0(BH = 0) 蓝底粉红字(BL = 1fh)
-   mov	 dx, 0x1800		 ;
-   int	 0x10                    ; 10h 号中断
-
-;----------------------------------------   准备进入保护模式   ------------------------------------------
-									;1 打开A20
-									;2 加载gdt
-									;3 将cr0的pe位置1
-
-
-   ;-----------------  打开A20  ----------------
-   in al,0x92
-   or al,0000_0010B
-   out 0x92,al
-
-   ;-----------------  加载GDT  ----------------
-   lgdt [gdt_ptr]
-
-
-   ;-----------------  cr0第0位置1  ----------------
-   mov eax, cr0
-   or eax, 0x00000001
-   mov cr0, eax
-
-   ;jmp dword SELECTOR_CODE:p_mode_start	     ; 刷新流水线，避免分支预测的影响,这种cpu优化策略，最怕jmp跳转，
-   jmp  SELECTOR_CODE:p_mode_start	     ; 刷新流水线，避免分支预测的影响,这种cpu优化策略，最怕jmp跳转，
-					     ; 这将导致之前做的预测失效，从而起到了刷新的作用。
+   TI_GDT equ  0
+RPL0  equ   0
+SELECTOR_VIDEO equ (0x0003<<3) + TI_GDT + RPL0
 
 [bits 32]
-p_mode_start:
-   mov ax, SELECTOR_DATA
-   mov ds, ax
-   mov es, ax
-   mov ss, ax
-   mov esp,LOADER_STACK_TOP
-   mov ax, SELECTOR_VIDEO
+section .text
+;------------------------   put_char   -----------------------------
+;功能描述:把栈中的1个字符写入光标所在处
+;-------------------------------------------------------------------   
+global put_char
+put_char:
+   pushad	   ;备份32位寄存器环境
+   ;需要保证gs中为正确的视频段选择子,为保险起见,每次打印时都为gs赋值
+   mov ax, SELECTOR_VIDEO	       ; 不能直接把立即数送入段寄存器
    mov gs, ax
 
-   mov byte [gs:160], 'P'
+;;;;;;;;;  获取当前光标位置 ;;;;;;;;;
+   ;先获得高8位
+   mov dx, 0x03d4  ;索引寄存器
+   mov al, 0x0e	   ;用于提供光标位置的高8位
+   out dx, al
+   mov dx, 0x03d5  ;通过读写数据端口0x3d5来获得或设置光标位置 
+   in al, dx	   ;得到了光标位置的高8位
+   mov ah, al
 
-   jmp $
+   ;再获取低8位
+   mov dx, 0x03d4
+   mov al, 0x0f
+   out dx, al
+   mov dx, 0x03d5 
+   in al, dx
+
+   ;将光标存入bx
+   mov bx, ax	  
+   ;下面这行是在栈中获取待打印的字符
+   mov ecx, [esp + 36]	      ;pushad压入4×8＝32字节,加上主调函数的返回地址4字节,故esp+36字节
+   cmp cl, 0xd				  ;CR是0x0d,LF是0x0a
+   jz .is_carriage_return
+   cmp cl, 0xa
+   jz .is_line_feed
+
+   cmp cl, 0x8				  ;BS(backspace)的asc码是8
+   jz .is_backspace
+   jmp .put_other	   
+;;;;;;;;;;;;;;;;;;
+
+ .is_backspace:		      
+;;;;;;;;;;;;       backspace的一点说明	     ;;;;;;;;;;
+; 当为backspace时,本质上只要将光标移向前一个显存位置即可.后面再输入的字符自然会覆盖此处的字符
+; 但有可能在键入backspace后并不再键入新的字符,这时在光标已经向前移动到待删除的字符位置,但字符还在原处,
+; 这就显得好怪异,所以此处添加了空格或空字符0
+   dec bx
+   shl bx,1
+   mov byte [gs:bx], 0x20		  ;将待删除的字节补为0或空格皆可
+   inc bx
+   mov byte [gs:bx], 0x07
+   shr bx,1
+   jmp .set_cursor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ .put_other:
+   shl bx, 1				  ; 光标位置是用2字节表示,将光标值乘2,表示对应显存中的偏移字节
+   mov [gs:bx], cl			  ; ascii字符本身
+   inc bx
+   mov byte [gs:bx],0x07		  ; 字符属性
+   shr bx, 1				  ; 恢复老的光标值
+   inc bx				  ; 下一个光标值
+   cmp bx, 2000		   
+   jl .set_cursor			  ; 若光标值小于2000,表示未写到显存的最后,则去设置新的光标值
+					  ; 若超出屏幕字符数大小(2000)则换行处理
+ .is_line_feed:				  ; 是换行符LF(\n)
+ .is_carriage_return:			  ; 是回车符CR(\r)
+					  ; 如果是CR(\r),只要把光标移到行首就行了。
+   xor dx, dx				  ; dx是被除数的高16位,清0.
+   mov ax, bx				  ; ax是被除数的低16位.
+   mov si, 80				  ; 由于是效仿linux，linux中\n便表示下一行的行首，所以本系统中，
+   div si				  ; 把\n和\r都处理为linux中\n的意思，也就是下一行的行首。
+   sub bx, dx				  ; 光标值减去除80的余数便是取整
+					  ; 以上4行处理\r的代码
+
+ .is_carriage_return_end:		  ; 回车符CR处理结束
+   add bx, 80
+   cmp bx, 2000
+ .is_line_feed_end:			  ; 若是LF(\n),将光标移+80便可。  
+   jl .set_cursor
+
+;屏幕行范围是0~24,滚屏的原理是将屏幕的1~24行搬运到0~23行,再将第24行用空格填充
+ .roll_screen:				  ; 若超出屏幕大小，开始滚屏
+   cld  
+   mov ecx, 960				  ; 一共有2000-80=1920个字符要搬运,共1920*2=3840字节.一次搬4字节,共3840/4=960次 
+   mov esi, 0xb80a0			  ; 第1行行首
+   mov edi, 0xb8000			  ; 第0行行首
+   rep movsd				  
+
+;;;;;;;将最后一行填充为空白
+   mov ebx, 3840			  ; 最后一行首字符的第一个字节偏移= 1920 * 2
+   mov ecx, 80				  ;一行是80字符(160字节),每次清空1字符(2字节),一行需要移动80次
+ .cls:
+   mov word [gs:ebx], 0x0720		  ;0x0720是黑底白字的空格键
+   add ebx, 2
+   loop .cls 
+   mov bx,1920				  ;将光标值重置为1920,最后一行的首字符.
+
+ .set_cursor:   
+					  ;将光标设为bx值
+;;;;;;; 1 先设置高8位 ;;;;;;;;
+   mov dx, 0x03d4			  ;索引寄存器
+   mov al, 0x0e				  ;用于提供光标位置的高8位
+   out dx, al
+   mov dx, 0x03d5			  ;通过读写数据端口0x3d5来获得或设置光标位置 
+   mov al, bh
+   out dx, al
+
+;;;;;;; 2 再设置低8位 ;;;;;;;;;
+   mov dx, 0x03d4
+   mov al, 0x0f
+   out dx, al
+   mov dx, 0x03d5 
+   mov al, bl
+   out dx, al
+ .put_char_done: 
+   popad
+   ret
